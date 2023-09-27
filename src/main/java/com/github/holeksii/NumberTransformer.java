e3cff1de7bc57c0644fefdd9e07adabd626e498f
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2023-09-27 23:32:12 +0300

    Implement number transformation

---
+++ src/main/java/com/github/holeksii/NumberTransformer.java
@@ -0,0 +1,27 @@
+package com.github.holeksii;
+
+public class NumberTransformer {
+
+  public int transform(int num) {
+    if (num == 0) {
+      return 1;
+    }
+
+    int result = 0;
+    int multiplier = 1;
+    while (num > 0) {
+      int digit = num % 10;
+      num /= 10;
+
+      // If digit is 9, add 10 to the result
+      if (digit == 9) {
+        result += (10 * multiplier);
+        multiplier *= 100; // 10 for the 0 and another 10 for the 1 in the next iteration
+      } else {
+        result += ((digit + 1) * multiplier);
+        multiplier *= 10;
+      }
+    }
+    return result;
+  }
+}
---
+++ src/test/java/com/github/holeksii/NumberTransformerTest.java
@@ -0,0 +1,51 @@
+package com.github.holeksii;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumberTransformerTest {
+
+  @Test
+  public void testSingleDigitTransformation() {
+    NumberTransformer transformer = new NumberTransformer();
+
+    assertEquals(1, transformer.transform(0));
+    assertEquals(2, transformer.transform(1));
+    assertEquals(3, transformer.transform(2));
+    assertEquals(10, transformer.transform(9));
+  }
+
+  @Test
+  public void testMultipleDigitTransformation() {
+    NumberTransformer transformer = new NumberTransformer();
+
+    assertEquals(10109, transformer.transform(998));
+    assertEquals(456, transformer.transform(345));
+    assertEquals(21010, transformer.transform(199));
+  }
+
+  @Test
+  public void testAllNinesTransformation() {
+    NumberTransformer transformer = new NumberTransformer();
+
+    assertEquals(10, transformer.transform(9));
+    assertEquals(1010, transformer.transform(99));
+    assertEquals(101010, transformer.transform(999));
+  }
+
+  @Test
+  public void testZeroTransformation() {
+    NumberTransformer transformer = new NumberTransformer();
+
+    assertEquals(1, transformer.transform(0));
+  }
+
+  @Test
+  public void testRandomTransformation() {
+    NumberTransformer transformer = new NumberTransformer();
+
+    assertEquals(89, transformer.transform(78));
+    assertEquals(54321, transformer.transform(43210));
+    assertEquals(2211, transformer.transform(1100));
+  }
+}
