6f7c0c6b5787d6b4253b123ce4e35a457c5fc756
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2023-09-28 12:58:38 +0300

    Add example how to sort a list of shapes

--- src/main/java/com/github/holeksii/Main.java
+++ src/main/java/com/github/holeksii/Main.java
@@ -1,7 +1,23 @@
 package com.github.holeksii;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.github.holeksii.shapes.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Shape> shapes = new ArrayList<>(List.of(
+            new Circle(5),
+            new Rectangle(5, 10),
+            new Square(5),
+            new Triangle(5, 10)
+        ));
+
+        shapes.forEach(s -> System.out.println(s.getArea()));
+        System.out.println();
+
+        shapes.sort(Shape.comparator);
+
+        shapes.forEach(s -> System.out.println(s.getArea()));
     }
 }
